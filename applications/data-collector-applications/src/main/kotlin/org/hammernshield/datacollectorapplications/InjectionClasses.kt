041d8f0f288a6514943c6c0bcb1bc38fcb271336
Austin Townsend <Austintownsend1234@gmail.com>
2024-08-09 10:47:44 -0700

    Update API key injection and add CI/CD pipeline
    Replaced hardcoded API key with a configurable value in `InjectionClasses.kt`. Added `.gitignore` entry for `data.env` and updated IDE configuration files. Introduced a new CI/CD workflow for automatic deployments to EC2.

--- applications/data-collector-applications/src/main/kotlin/org/hammernshield/datacollectorapplications/InjectionClasses.kt
+++ applications/data-collector-applications/src/main/kotlin/org/hammernshield/datacollectorapplications/InjectionClasses.kt
@@ -1,15 +1,18 @@
 package org.hammernshield.datacollectorapplications
 
 import org.hammernshield.datacollector.AirQualityCollector
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class InjectionClasses {
 
+    @Value("\${air.quality.api.key}")
+    private lateinit var apiKey: String
+
     @Bean
     fun provideDataCollectorClient(): AirQualityCollector {
-        return AirQualityCollector("DudkR2rZIOz9EAmHGqcR4w==eZCXO2M1e9769tgq")
+        return AirQualityCollector(apiKey)
     }
-
 }
